004cd9e2c566a005ec5abbd5ca5dc6163b3fe6b8
dawez <dawez1@gmail.com>
2014-07-23 13:12:30 +0200

    adding onResume event

--- www/js/index.js
+++ www/js/index.js
@@ -27,6 +27,7 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
+        document.addEventListener("resume", this.onResume, false);
     },
     // deviceready Event Handler
     //
@@ -35,6 +36,15 @@ var app = {
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
     },
+
+    onResume: function () {
+        if (window.cordova && typeof facebookConnectPlugin != 'undefined') {
+            var fb_success = function (data) {};
+            var fb_fail = function (data) {};
+            facebookConnectPlugin.activateApp(fb_success, fb_fail);
+        }
+    },
+
     // Update DOM on a Received Event
     receivedEvent: function(id) {
         var parentElement = document.getElementById(id);
